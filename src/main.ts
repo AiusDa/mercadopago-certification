ff70268b42a2e26e13d8aea3a11d50cb5a0d95ce
Adrián Darío Hidalgo Flores <ahidalgo@idealizer.mx>
2020-10-28 21:37:33 -0600

    Cors enabled

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const PORT = process.env.PORT || 5000;
   await app.listen(PORT).then(() => console.log(`App Running on port ${PORT}`));
 }
